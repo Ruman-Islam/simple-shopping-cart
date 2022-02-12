6d7dd9551d51881f01973eda9bd4649ee0b2ad17
Ruman-Islam <rumanislam48@gmail.com>
2022-02-12 23:02:00 +0600

    previous code pushed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,13 @@
 
 function phoneIncreaseDecrease(plusMinus, categoryQuantity, category) {
     const productQuantityInput = document.getElementById(categoryQuantity + '__input');
-    const productQuantityValue = parseInt(productQuantityInput.value);
+    const productQuantityValue = parseInt(phoneQuantityInput.value);
     const productPrice = document.getElementById(category + '__price');
     if (plusMinus === 'increase') {
         const productQuantity = productQuantityValue + 1;
         productQuantityInput.value = productQuantity;
-        if (category === 'phone') {
-            const productTotalPrice = productQuantity * 1219;
-            productPrice.innerText = productTotalPrice;
-        } else if (category === 'case') {
-            const productTotalPrice = productQuantity * 59;
-            productPrice.innerText = productTotalPrice;
-        }
+        const productTotalPrice = productQuantity * 1219;
+        productPrice.innerText = productTotalPrice;
     } else if (plusMinus === 'decrease') {
         if (productQuantityValue > 0) {
             const phoneQuantity = productQuantityValue - 1;
