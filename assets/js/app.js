93fc6d4ee655fd24182b7c26ae6727c34ed9154a
Ruman-Islam <rumanislam48@gmail.com>
2022-02-12 23:13:40 +0600

    only individual product total price calculation part done & subtotal, tax & grandtotal part remains

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,29 @@
 
 function phoneIncreaseDecrease(plusMinus, categoryQuantity, category) {
     const productQuantityInput = document.getElementById(categoryQuantity + '__input');
-    const productQuantityValue = parseInt(phoneQuantityInput.value);
+    const productQuantityValue = parseInt(productQuantityInput.value);
     const productPrice = document.getElementById(category + '__price');
     if (plusMinus === 'increase') {
         const productQuantity = productQuantityValue + 1;
         productQuantityInput.value = productQuantity;
-        const productTotalPrice = productQuantity * 1219;
-        productPrice.innerText = productTotalPrice;
+        if (category === 'phone') {
+            const productTotalPrice = productQuantity * 1219;
+            productPrice.innerText = productTotalPrice;
+        } else if (category === 'case') {
+            const productTotalPrice = productQuantity * 59;
+            productPrice.innerText = productTotalPrice;
+        }
     } else if (plusMinus === 'decrease') {
         if (productQuantityValue > 0) {
-            const phoneQuantity = productQuantityValue - 1;
-            productQuantityInput.value = phoneQuantity;
-            const phoneTotalPrice = phoneQuantity * 1219;
-            productPrice.innerText = phoneTotalPrice;
+            const productQuantity = productQuantityValue - 1;
+            productQuantityInput.value = productQuantity;
+            if (category === 'phone') {
+                const productTotalPrice = productQuantity * 1219;
+                productPrice.innerText = productTotalPrice;
+            } else if (category === 'case') {
+                const productTotalPrice = productQuantity * 59;
+                productPrice.innerText = productTotalPrice;
+            }
         }
     }
 }
