01ab09f8f60b5298d466b13fd6b70d4db6be8134
Ruman-Islam <rumanislam48@gmail.com>
2022-02-12 22:59:29 +0600

    code optimized

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+
+function phoneIncreaseDecrease(plusMinus, categoryQuantity, category) {
+    const productQuantityInput = document.getElementById(categoryQuantity + '__input');
+    const productQuantityValue = parseInt(productQuantityInput.value);
+    const productPrice = document.getElementById(category + '__price');
+    if (plusMinus === 'increase') {
+        const productQuantity = productQuantityValue + 1;
+        productQuantityInput.value = productQuantity;
+        if (category === 'phone') {
+            const productTotalPrice = productQuantity * 1219;
+            productPrice.innerText = productTotalPrice;
+        } else if (category === 'case') {
+            const productTotalPrice = productQuantity * 59;
+            productPrice.innerText = productTotalPrice;
+        }
+    } else if (plusMinus === 'decrease') {
+        if (productQuantityValue > 0) {
+            const phoneQuantity = productQuantityValue - 1;
+            productQuantityInput.value = phoneQuantity;
+            const phoneTotalPrice = phoneQuantity * 1219;
+            productPrice.innerText = phoneTotalPrice;
+        }
+    }
+}
